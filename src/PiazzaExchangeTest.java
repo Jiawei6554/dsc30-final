304523072f8858609ea784d24cb4074af2bfb163
杨嘉唯 <jiaweiyanghz@gmail.com>
2022-03-14 15:25:05 -0700

    updated starter code

---
+++ src/PiazzaExchangeTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PiazzaExchangeTest {
+
+    @Test
+    void Test() {
+        Question Q= new Question(null, "a", "s");
+    }
+}
